ea6d09404254ac98ca54bc21f638bfdcc181ad00
HeyGit12 <seymen544@gmail.com>
2021-06-25 01:21:22 +0200

    Before and after methods added

--- src/test/java/GitHubProject/groupStudy.java
+++ src/test/java/GitHubProject/groupStudy.java
@@ -1,4 +1,25 @@
 package GitHubProject;
 
+import Utility.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
 public class groupStudy {
+    WebDriver driver;
+    @BeforeMethod
+    public void setup(){
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+    }
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
+    }
+
+
+
+
+
+
 }
